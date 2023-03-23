000002f7e122e1d7aed947105129cddffb7bec0d
Roman <rmnkcs@gmail.com>
2023-03-23 23:03:07 +0100

    Change router hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import TodoListsView from "../views/TodoListsView.vue";
 import TodoListView from '../views/TodoListView.vue';
 
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
